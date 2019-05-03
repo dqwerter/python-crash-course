fd0031c3f189faead1af9ad60b484f4d592f2f25
dqwerter <danielqin7@outlook.com>
2019-05-03 15:39:00 +0800

    AttributeError solved.

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,6 +1,7 @@
-import sys
 import pygame
 from settings import Settings
+from ship import Ship
+import game_fuctions as gf
 
 
 def run_game():
@@ -11,16 +12,16 @@ def run_game():
         (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption("Alien Invasion")
 
+    ship = Ship(ai_settings, screen)
     # start the main loop for the game
     while True:
 
         # redraw the screen during each pass though the loop
         screen.fill(ai_settings.bg_color)
 
-        # Watch for Keyboard  and mouse events
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                    sys.exit()
+        gf.check_events(ship)
+        ship.update()
+        ship.blitme()
 
         # make the most recently drawn screen visible
         pygame.display.flip()
