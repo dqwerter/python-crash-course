3ac0ee4b8cd2e00db785747761faf68d88ae68fd
dqwerter <danielqin7@outlook.com>
2019-05-03 13:45:27 +0800

    AttributeError: type object 'Settings' has no attribute 'screen_width'

--- alien_invasion/alien_invasion.py
+++ alien_invasion/alien_invasion.py
@@ -1,16 +1,22 @@
 import sys
 import pygame
-
+from settings import Settings
 
 def run_game():
     pygame.init()
-    screen = pygame.display.set_mode((1200, 800))
+    # ai_settings = Settings
+    screen = pygame.display.set_mode(
+        (Settings.screen_width, Settings.screen_height)
+    )
     pygame.display.set_caption("Alien Invasion")
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+
+        screen.fill(Settings.bg_color)
+
         pygame.display.flip()
 
 
